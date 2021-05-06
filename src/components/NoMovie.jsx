ca27c86e09197de71dd02a38cee4e2ebfb17f69f
Jang <sbtan1989@gmail.com>
2021-05-06 16:25:18 -0700

    add img for no moive and no nomination components

--- src/components/NoMovie.jsx
+++ src/components/NoMovie.jsx
@@ -1,9 +1,11 @@
-import { Typography } from "@material-ui/core"
+import { Typography } from "@material-ui/core";
+import TheaterImg from '../Images/movie_theater.jpg';
 
 export default function NoMovie() {
   return (
-    <>
-    <Typography>No Movies</Typography>
-    </>
+    <div className="no-container">
+      <img className="no-img" src={TheaterImg} alt="NomovieImg" />
+      <Typography style={{ position: "absolute" }} align="center" variant="h3">No Movies!</Typography>
+    </div>
   );
 }
--- src/components/NoNomination.jsx
+++ src/components/NoNomination.jsx
@@ -1,7 +1,11 @@
 import { Typography } from "@material-ui/core"
+import awardImg from "../Images/award.jpg"
 
 export default function NoNomination() {
   return (
-    <Typography>Please Add to Moive for Nomination</Typography>
+    <div className="no-container">
+      <img className="no-img" src={awardImg} alt="noNomiantionImg" />
+      <Typography style={{ position: "absolute" }} variant="h3" >Please Add Moive for Nomination</Typography>
+    </div>
   );
 }
