27ed0fae2946627bc4ca8523d30fa15f53e134d1
Jang <sbtan1989@gmail.com>
2021-05-06 16:59:27 -0700

    Updates

--- src/components/SectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-import { Grid, Typography } from '@material-ui/core';
-
-export default function SectionTitle({ title }) {
-  return (
-    <Grid item sm="4">
-      <Typography variant="h3">{title}</Typography>
-    </Grid>
-  );
-}
--- src/utilities/OMDbAPI.js
+++
@@ -1,5 +0,0 @@
-
-export async function getMoives (searchVal) {
-  const url = `http://www.omdbapi.com/?s=${searchVal}&apikey=f135536d`;
-  return await (await fetch(url)).json();
-}
